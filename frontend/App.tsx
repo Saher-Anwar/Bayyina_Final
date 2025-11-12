467a0f2116f8245c609d04e66b34d52eb1060600
Saher <saher.ziauddin@dal.ca>
2025-11-12 00:03:05 -0400

    Added other gluestack components

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,18 +1,24 @@
-import { StatusBar } from 'expo-status-bar';
 import { GluestackUIProvider } from '@/components/ui/gluestack-ui-provider';
+import { useState } from 'react';
+import { Box } from '@/components/ui/box';
+import { Button, ButtonText } from '@/components/ui/button';
 
 import './global.css';
-import { Text } from 'react-native';
 
 export default function App() {
+  const [colorMode, setColorMode] = useState<'light' | 'dark'>('light');
+
   return (
-    
-    <GluestackUIProvider mode="dark">
-      <>
-      <Text>Testing</Text>
-      <StatusBar style="auto" />
-    </>
+    <GluestackUIProvider mode={colorMode}>
+      <Box className="bg-primary-950 flex-1 justify-center items-center">
+        <Button
+          onPress={() => {
+            setColorMode(colorMode === 'light' ? 'dark' : 'light');
+          }}
+        >
+          <ButtonText>Toggle color mode</ButtonText>
+        </Button>
+      </Box>
     </GluestackUIProvider>
-  
   );
 }
