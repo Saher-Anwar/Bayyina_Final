7bbc4488b63e6a1b6ac611225e28bfbf80c383d5
Saher <saher.ziauddin@dal.ca>
2025-11-12 00:11:47 -0400

    Implemented dark mode persistence

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -4,21 +4,24 @@ import { Box } from '@/components/ui/box';
 import { Button, ButtonText } from '@/components/ui/button';
 
 import './global.css';
+import { ThemeProvider } from '@/components/ui/ThemeProvider/ThemeProvider';
 
 export default function App() {
   const [colorMode, setColorMode] = useState<'light' | 'dark'>('light');
 
   return (
-    <GluestackUIProvider mode={colorMode}>
-      <Box className="bg-primary-950 flex-1 justify-center items-center">
-        <Button
-          onPress={() => {
-            setColorMode(colorMode === 'light' ? 'dark' : 'light');
-          }}
-        >
-          <ButtonText>Toggle color mode</ButtonText>
-        </Button>
-      </Box>
-    </GluestackUIProvider>
+    <ThemeProvider>
+      <GluestackUIProvider mode={colorMode}>
+        <Box className="bg-primary-950 flex-1 justify-center items-center">
+          <Button
+            onPress={() => {
+              setColorMode(colorMode === 'light' ? 'dark' : 'light');
+            }}
+          >
+            <ButtonText>Toggle color mode</ButtonText>
+          </Button>
+        </Box>
+      </GluestackUIProvider>
+    </ThemeProvider>
   );
 }
