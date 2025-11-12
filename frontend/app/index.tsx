2c6972d9d1416eca094ac6dba539a45e3820f2dc
Saher <saher.ziauddin@dal.ca>
2025-11-12 02:27:17 -0400

    Fixed styling issue

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -1,13 +1,15 @@
 import { View, Text, Button } from 'react-native';
 import { useTheme } from '@/components/ui/ThemeProvider/ThemeProvider';
+import { Link } from 'expo-router';
 
 export default function Home() {
   const { theme, toggleTheme } = useTheme();
 
   return (
     <View className="flex-1 justify-center items-center bg-primary-950">
-      <Text className="bg-error-200 mb-4">Home Screen</Text>
+      <Text className="text-secondary-0 mb-4">Home Screen</Text>
       <Button title={`Toggle Theme (${theme})`} onPress={toggleTheme} />
+      <Link className='text-secondary-0' href={'/settings'}>Settings</Link>
     </View>
   );
 }
--- frontend/app/settings.tsx
+++ frontend/app/settings.tsx
@@ -1,10 +1,13 @@
-// src/app/settings.tsx
 import { View, Text } from 'react-native';
+import { Link } from 'expo-router';
 
 export default function Settings() {
   return (
-    <View className="flex-1 justify-center items-center bg-primary-950">
-      <Text className="text-white">Settings Screen</Text>
+    <View className="flex-1 justify-center items-center bg-red-500">
+      <Text className="text-secondary-0 text-2xl mb-4">Settings Screen</Text>
+      <Link href="/">
+        <Text className="text-secondary-0 underline">Go Back Home</Text>
+      </Link>
     </View>
   );
 }
